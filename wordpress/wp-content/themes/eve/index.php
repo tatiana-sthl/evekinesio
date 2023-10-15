b0263d81f96f401548c5b1a587325f331a007425
tatiana <stahli.tatiana@gmail.com>
2023-10-15 16:10:13 +0200

    add minimum about presentation

--- wordpress/wp-content/themes/eve/index.php
+++ wordpress/wp-content/themes/eve/index.php
@@ -1,5 +1,25 @@
 <?php get_header(); ?>
 
-<h1>Coucou</h1>
+<div class="presentation">
+  <img src="http://evekinesio.fr/wp-content/uploads/2023/10/imglogo-removebg-preview.png" alt="Logo" class="logo">
+  <div class="textPresentation">
+    <h1 class="h1Eve">Eve Girardot</h1>
+    <h2>Kinésiologie - Humains et Animaux<br>Naturopathie animalière</h2> 
+    <img src="http://evekinesio.fr/wp-content/uploads/2023/10/Capture-decran-2023-10-12-192146.png" alt='Eve' class="imgJaffa">
+    <div class="paragrapheKinesio">
+      
+      <h3> La Kinésiologie c’est quoi et qui suis-je ? </h3>
+      <p>
+Un moment que je consacre à votre animal, ou à vous-même, afin de débloquer certaines zones parfois douloureuses tant émotionnellement que physiologiquement.<br>
+
+Imaginez un psy, puis imaginez-le en train de faire des tests sur vos muscles plutôt que de vous faire parler en continu. En enfin imaginez des réequilibrages adaptés à ce que votre corps désire vraiment et voilà, vous avez une brève idée de ce qu’est la Kinésiologie.<br>
+
+Et moi, je suis simplement une amoureuse des animaux depuis toujours,. Diplômée à la base dans le domaine équestre j’ai finalement perdu de vue ce monde cher à mes yeux quelque temps. Et cela m’a fait comprendre une chose... que je voulais retourner auprès d’eux et de manière efficace pour leur bien-être.<br></p>
+      
+    </div>
+    
+  </div>
+  
+</div>
 
 <?php get_footer(); ?>

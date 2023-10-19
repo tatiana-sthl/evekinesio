06d86a8a36dbcf11e034380a08cf4de671d06440
tatiana <stahli.tatiana@gmail.com>
2023-10-19 11:02:22 +0200

    add all files in prod

--- wordpress/wp-content/themes/eve/footer.php
+++ wordpress/wp-content/themes/eve/footer.php
@@ -1,5 +1,5 @@
 <footer class="site__footer">
-    <?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+    
     <p class="menu-bas-de-page">&copy;2023 - eve.kinesio - Tous droits réservés</p>
 </footer>
 

124207d97fe70d34267f8a6ad37c3a9c4becf4e1
tatiana <stahli.tatiana@gmail.com>
2023-10-27 17:09:41 +0200

    finish first div pratiques

--- wordpress/wp-content/themes/eve/index.php
+++ wordpress/wp-content/themes/eve/index.php
@@ -62,8 +62,8 @@ Lorsque nous mettons des mots sur des maux, les dits maux deviennent des mots di
       <p>
         Que l'on appartienne au monde animal ou au monde des hommes, voilà le principe même de la kinésiologie : trouver les mots pour les maux. Mais il est évident que les mots sont parfois difficiles à trouver car on ne "sait" tout simplement pas d'où provient notre mal.L'avantage de la kinésio c'est que l'on interroge pas en premier le "savoir", le cerveau. Non, on interroge plutôt la mémoire musculaire.Notre corps enregistre tout. Il se souvient de tout même si le cerveau l'a "oublié". Et c'est grâce à cette mémoire musculaire que l'on retrouve les mots qui nous ont blessé.Une fois retrouvés, il devient alors, plus facile de libérer le corps et l'esprit pour tendre vers le mieux-être.
       </p>
+      <h3>Pour quelles raisons faire une séance ?</h3>
       <div class="raisons">
-        <h3>Pour quelles raisons faire une séance ?</h3>
         <ul class="raisonsListe">
           <li>Gestion du stress</li>
           <li>Adaptation aux changements / angoisses de séparation</li>

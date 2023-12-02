bb8dc28dbde657cd0487241d1c7ccd737602c00c
tatiana <stahli.tatiana@gmail.com>
2023-12-02 09:06:31 +0100

    add calendly

--- wordpress/wp-content/themes/eve/index.php
+++ wordpress/wp-content/themes/eve/index.php
@@ -132,6 +132,23 @@
   </div>
 </div>
 
+<div class="rdv">
+  <h1>Prendre rendez-vous</h1>
+  <!-- Début de widget en ligne Calendly -->
+  <div class="calendly-inline-widget" data-url="https://calendly.com/evegirardot/seance-de-kinesiologie?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=71a152" style="min-width:320px;height:700px;"></div>
+  <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+  <!-- Fin de widget en ligne Calendly -->
+  <!-- Début de widget en ligne Calendly -->
+<div class="calendly-inline-widget" data-url="https://calendly.com/evegirardot/seance-de-kinesiologie-troyes?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=dbc472" style="min-width:320px;height:700px;"></div>
+<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+<!-- Fin de widget en ligne Calendly -->
+<!-- Début de widget en ligne Calendly -->
+<div class="calendly-inline-widget" data-url="https://calendly.com/evegirardot/seance-de-kinesiologie-dijon?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=333333" style="min-width:320px;height:700px;"></div>
+<script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+<!-- Fin de widget en ligne Calendly -->
+</div>
+
+
 
 
 <!-- <div class="contact">

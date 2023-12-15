d7ea6f84e5015e8a97ba4a6d11f2c58128d3598d
tatiana <stahli.tatiana@gmail.com>
2023-12-15 11:56:20 +0100

    modify style presentation

--- wordpress/wp-content/themes/eve/index.php
+++ wordpress/wp-content/themes/eve/index.php
@@ -32,26 +32,25 @@
 <div id="presentation" class="presentation">
   <img src="http://evekinesio.fr/wp-content/uploads/2023/10/imglogo-removebg-preview.png" alt="Logo" class="logo">
   <div class="textPresentation">
+    <img src="http://evekinesio.fr/wp-content/uploads/2023/10/logo_eve__1_-removebg-preview.png" alt='Eve' class="imgJaffa">
     <div class="textPresentationTitre">
       <div>
         <h1 class="h1Eve">Eve Girardot</h1>
         <h2 class="h2Eve">Kinésiologie - Humains et Animaux<br>Naturopathie animalière</h2> 
       </div>
-      <img src="http://evekinesio.fr/wp-content/uploads/2023/10/logo_eve__1_-removebg-preview.png" alt='Eve' class="imgJaffa">
-    </div>
-    
-    <div class="paragrapheKinesio">
-      <h3 class="h3Eve"> La Kinésiologie c’est quoi et qui suis-je ? </h3>
-      <p class="pEve">
-        Un moment que je consacre à votre animal, ou à vous-même, afin de débloquer certaines zones parfois douloureuses tant émotionnellement que physiologiquement.<br>
-
-        Imaginez un psy, puis imaginez-le en train de faire des tests sur vos muscles plutôt que de vous faire parler en continu. En enfin imaginez des réequilibrages adaptés à ce que votre corps désire vraiment et voilà, vous avez une brève idée de ce qu’est la Kinésiologie.<br>
-
-        Et moi, je suis simplement une amoureuse des animaux depuis toujours,. Diplômée à la base dans le domaine équestre j’ai finalement perdu de vue ce monde cher à mes yeux quelque temps. Et cela m’a fait comprendre une chose... que je voulais retourner auprès d’eux et de manière efficace pour leur bien-être.<br></p>
-    </div>
-    
+    </div>  
   </div>
-  
+</div>
+
+    
+<div class="paragrapheKinesio">
+  <h3 class="h3Eve"> La Kinésiologie c’est quoi et qui suis-je ? </h3>
+  <p class="pEve">
+    Un moment que je consacre à votre animal, ou à vous-même, afin de débloquer certaines zones parfois douloureuses tant émotionnellement que physiologiquement.<br>
+
+    Imaginez un psy, puis imaginez-le en train de faire des tests sur vos muscles plutôt que de vous faire parler en continu. En enfin imaginez des réequilibrages adaptés à ce que votre corps désire vraiment et voilà, vous avez une brève idée de ce qu’est la Kinésiologie.<br>
+
+    Et moi, je suis simplement une amoureuse des animaux depuis toujours,. Diplômée à la base dans le domaine équestre j’ai finalement perdu de vue ce monde cher à mes yeux quelque temps. Et cela m’a fait comprendre une chose... que je voulais retourner auprès d’eux et de manière efficace pour leur bien-être.<br></p>
 </div>
 
 <blockquote class="otro-blockquote">

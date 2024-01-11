e609803c0e442436cdf17381dad443d022524164
tatiana <stahli.tatiana@gmail.com>
2024-01-11 18:15:12 +0100

    finally add a new page about legal mention

--- wordpress/wp-content/themes/eve/mentions.php
+++ wordpress/wp-content/themes/eve/mentions.php
@@ -1,6 +1,19 @@
 
-<?php get_header(); ?>
+<?php 
+
+/*
+Template Name: Mentions Légales
+*/
+
+get_header(); ?>
 
 <h1>Mentions légales</h1>
+<p>Tests</p> 
 
-<?php get_footer(); ?>
+<footer class="site__footer">
+
+    <a href="http://localhost/evekinesio/wordpress/">Retourner sur le site</a>
+    <p class="menu-bas-de-page">&copy;2023 - eve.kinesio - Tous droits réservés</p>
+</footer>
+
+<?php wp_footer(); ?>

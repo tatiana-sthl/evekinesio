90b85df51a182a7f04aaa087f59f382438680170
tatiana <stahli.tatiana@gmail.com>
2024-01-11 18:26:40 +0100

    fix error about url

--- wordpress/wp-content/themes/eve/footer.php
+++ wordpress/wp-content/themes/eve/footer.php
@@ -1,6 +1,6 @@
 <footer class="site__footer">
 
-    <a href="http://localhost/evekinesio/wordpress/mentions-legales/">Mentions Légales</a>
+    <a href="http://evekinesio.fr/mentions-legales/">Mentions Légales</a>
     <p class="menu-bas-de-page">&copy;2023 - eve.kinesio - Tous droits réservés</p>
 </footer>
 

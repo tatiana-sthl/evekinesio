d93437960f8aa4cc276299b81916ebaefaad69eb
tatiana <stahli.tatiana@gmail.com>
2024-01-13 20:27:43 +0100

    start working on text in legal

--- wordpress/wp-content/themes/eve/mentions.php
+++ wordpress/wp-content/themes/eve/mentions.php
@@ -7,11 +7,31 @@ Template Name: Mentions Légales
 
 get_header(); ?>
 
-<h1>Mentions légales</h1>
-<p>Tests</p> 
+<h1 class="h1legal">Mentions légales</h1>
+
+<p class="textlegal">Eve GIRARDOT Kinésiologie <br>
+2, rue de Beaulieu 17138 PUILBOREAU <br>
+Par téléphone : +33666993837<br>
+Par email : eve.girardot@gmail.com<br>
+<br>
+Entrepreneur individuel <br>
+SIREN : 980218523</p>
+
+
+<h3 class="textlegal">1. Prix</h3>
+<p class="textlegal">Les prix de mes services sont indiqués en euros toutes taxes comprises (TVA et autres taxes applicables au jour de la commande), Sauf indication contraire et hors frais de traitement et d’expédition.<br>
+La société Eve GIRARDOT Kinésiologie se réserve le droit de modifier ses prix à tout moment, mais le produit sera facturé sur la base du tarif en vigueur au moment de la validation de la commande et sous réserve de disponibilité.</p>
+
+<h3 class="textlegal">2. Obligation de résultat</h3>
+<p class="textlegal">La société Eve GIRARDOT Kinésiologie ne dispose d’aucune obligation de résultats à la suite de la réalisation d’une de ses prestations. Elle s’engage cependant à mobiliser toutes les ressources dont elle dispose pour accomplir la prestation promise, sans garantie du résultat.</p>
+
+<h3 class="textlegal">3. Hébergement</h3>
+<p class="textlegal">Le Site est hébergé par la société Hostinger via WordPress<br>
+Gestionnaire du site : Tatiana STAHLI <br>
+Directeur et responsable de la publication / rédaction : Eve GIRARDOT<br>
+</p>
 
 <footer class="site__footer">
-
     <a class="return-button" href="http://evekinesio.fr">Retourner sur le site</a>
     <p class="menu-bas-de-page">&copy;2023 - eve.kinesio - Tous droits réservés</p>
 </footer>
